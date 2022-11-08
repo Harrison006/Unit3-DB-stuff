c5b335f653885117a8de586bd2ce547277f2cd05
Harrison006 <87344112+Harrison006@users.noreply.github.com>
2022-11-08 14:47:02 +1000

    new db

--- datastore.py
+++ datastore.py
@@ -1,158 +1,117 @@
 import sqlite3
-from unicodedata import name
-from unittest import result
-
-
-
 
 class Datastore:
+
     def __init__(self, db_file_name: str):
 
-        self.conn = sqlite3.connect(netflix.db)
-        self.cur = self.conn.cursor()
-
+        self.connection = sqlite3.connect(db_file_name)
+        self.cursor = self.connection.cursor()
 
         self.build_db()
 
     def __del__(self):
-        """
-        writes data from cache to drive then closes connection
-        """
-        self.conn.close()
-
+        self.connection.close()
 
     def build_db(self):
-        """
-        Makes DB file with tables
-        """
-        self.cur.execute(
+        self.cursor.execute(
             """
-            CREATE TABLE "rating" (
-            "rating_id"	INTEGER,
-            "name"	TEXT NOT NULL,
-            PRIMARY KEY("dir_id" AUTOINCREMENT)
+            CREATE TABLE rating_db(
+                rating_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
             )
             """
         )
-        """
-        Rating table made
-        """
-        self.cur.execute(
+        self.cursor.execute(
             """
-            CREATE TABLE "actor" (
-            "actor_id"	INTEGER,
-            "name"	TEXT,
-            PRIMARY KEY("actor_id" AUTOINCREMENT)
+            CREATE TABLE show_tb(
+                show_id TEXT PRIMARY KEY,
+                title TEXT NOT NULL,
+                type TEXT NOT NULL,
+                date_added TEXT,
+                release_year INTEGER NOT NULL CHECK(release_year > 1900),
+                rating INTERGER,
+                duration TEXT NOT NULL,
+                description TEXT,
+                FOREIGN KEY (rating) REFERENCES rating_tb(rating_id)
+            )
+            """
+        )       
+        self.cursor.execute(
+            """
+            CREATE TABLE director_tb(
+                dir_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
             )
             """
         )
-        self.cur.execute(
-            """
-            CREATE TABLE "director" (
-            "dir_id"	INTEGER,
-            "name"	TEXT NOT NULL,
-            PRIMARY KEY("dir_id" AUTOINCREMENT)
-            )
-            """
-        )
-        """
-        Makes Director Table
-        """
-        self.cur.execute(
-            """
-            CREATE TABLE "Show" (
-            "show_id"	INTEGER,
-            "name"	TEXT NOT NULL,
-            "TYPE"	TEXT NOT NULL,
-            "date_added"	TEXT,
-            "date_released"	INTEGER,
-            "Duration"	TEXT,
-            "Description"	TEXT,
-            PRIMARY KEY("show_id" AUTOINCREMENT)
-            )
-            """
-        )
-        """
-        Makes Show table
-        """
-        self.cur.execute(
-            """
-            CREATE TABLE "country" (
-            "country_id"	INTEGER,
-            "name"	TEXT,
-            PRIMARY KEY("country_id" AUTOINCREMENT)
-            ) 
-            """
-        )
-        """
-        Makes Country table
-        """
-        self.cur.execute(
-            """
-            CREATE TABLE "catagory" (
-            "cat_id"	INTEGER,
-            "name"	TEXT,
-            PRIMARY KEY("cat_id" AUTOINCREMENT)
-            )
-            """
-        )
-        """
-        Makes Catagory table
-        """
-        
-        # Join tables
-
-        self.cur.execute(
+        self.cursor.execute(
             """
             CREATE TABLE show_director(
                 show_id TEXT NOT NULL,
                 dir_id INTEGER NOT NULL,
                 PRIMARY KEY (show_id, dir_id)
-                FOREIGN KEY (show_id) REFERENCES Show(show_id)
-                FOREIGN KET (dir_id) REFERENCES director(dir_id)
+                FOREIGN KEY (show_id) REFERENCES show_tb(show_id)
+                FOREIGN KEY (dir_id) REFERENCES director_tb(dir_id)
             )
             """
         )
-        self.cur.execute(
+        self.cursor.execute(
             """
-            CREATE TABLE cast(
+            CREATE TABLE actor_tb(
+                actor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+            """
+        )
+        self.cursor.execute(
+            """
+            CREATE TABLE cast_tb(
                 show_id TEXT NOT NULL,
                 actor_id INTEGER NOT NULL,
-                PRIMARY KEY (show_id, actor_id),
-                FOREIGN KEY (show_id) REFERENCES Show(show_id)
-                FOREIGN KEY (actor_id) REFERENCES actor(actor_id)
+                PRIMARY KEY (show_id, actor_id)
+                FOREIGN KEY (show_id) REFERENCES show_tb(show_id)
+                FOREIGN KEY (actor_id) REFERENCES actor_tb(actor_id)
             )
             """
         )
-        self.cur.execute(
+        self.cursor.execute(
             """
-            CREATE TABLE show_country(
+            CREATE TABLE country_tb(
+                country_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+            """
+        )
+        self.cursor.execute(
+            """
+            CREATE TABLE show_country_tb(
                 show_id TEXT NOT NULL,
-                actor_id INTEGER NOT NULL,
-                PRIMARY KEY (show_id, country_id),
-                FOREIGN KEY (show_id) REFERENCES Show(show_id)
-                FOREIGN KEY (country_id) REFERENCES country(country_id)
+                country_id INTEGER NOT NULL,
+                PRIMARY KEY (show_id, country_id)
+                FOREIGN KEY (show_id) REFERENCES show_tb(show_id)
+                FOREIGN KEY (country_id) REFERENCES country_tb(country_id)
             )
             """
         )
-        self.cur.execute(
+        self.cursor.execute(
             """
-            CREATE TABLE show_catagory(
+            CREATE TABLE category_tb(
+                cat_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+            """
+        )
+        self.cursor.execute(
+            """
+            CREATE TABLE show_category_tb(
                 show_id TEXT NOT NULL,
-                actor_id INTEGER NOT NULL,
-                PRIMARY KEY (show_id, cat_id),
-                FOREIGN KEY (show_id) REFERENCES Show(show_id)
-                FOREIGN KEY (cat_id) REFERENCES cat_id(cat_id)
+                cat_id INTEGER NOT NULL,
+                PRIMARY KEY (show_id, cat_id)
+                FOREIGN KEY (show_id) REFERENCES show_tb(show_id)
+                FOREIGN KEY (cat_id) REFERENCES category_tb(cat_id)
             )
             """
         )
 
-        self.conn.commit()
-# create methods
+        self.connection.commit()
 
-
-# get methods
-
-# update methods
-
-# delete methods
