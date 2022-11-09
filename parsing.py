d0c68b5733419acba24919a0ed77bd7a9b7a420b
Harrison <Harrison006@outlook.com.au>
2022-11-10 09:43:52 +1000

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -4,7 +4,9 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 
-with open(".\netflix_titles.csv", encoding="utf-8") as netflix_file:
-    csv_reader = csv.DictReader(netflix_file, delimiter=", ")
+with open("netflix_titles.csv", encoding="utf-8") as netflix_file:
+    csv_reader = csv.DictReader(netflix_file, delimiter=",")
 
-    print(csv_reader)
+
+    for row in csv_reader:
+        pp.pprint(row["rating"])
